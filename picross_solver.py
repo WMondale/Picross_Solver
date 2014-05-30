c7002e34ba9d920abce42ba046dc34b2588e472a
WMondale <wmcknig@g.clemson.edu>
2014-05-30 01:16:04 -0400

    Overhaul
    Overhauled to be much more method-centric.  Project currently stalled by
    an explicable changing of the "groups" variable in the "solset" method
    that occurs right after the s[n] assignment.

--- picross_solver.py
+++ picross_solver.py
@@ -1,76 +1,96 @@
 #picross_solver
-#Makes a 2D array of inputted dimensions, takes a list of positive integers for
-#each row and column of the array, and marks the array with blocks or blanks as #are logically confirmed to be certain
+#in the nonogram list, "-" represents undetermined spaces, " " represents confirmed blank spaces, and "O" represents confirmed blocks.  This means that, when comparing "isolation" solutions to the present row, it is acceptable for a mismatch to contain a "-"; it's the " "-"O" or vice-versa pairs that invalidate the solution
+#also, solutions do not contain "-"'s, so check through the row/column proper to screen out "O"-" " mismatch solutions
 
-#get dimensions for the picross array
+#data type validation method
+def validate(typ,msg):
+	var=input(msg)
+	try:
+		var=typ(var)
+	except ValueError:
+		print("Invalid data type")
+		while type(var) is not typ:
+			try:
+				var=typ(input(msg))
+			except ValueError:
+				print("Invalid data type.")
+	return var
+
+#number list metalist method
+def rnc(l,m,msg):
+	lst=[]
+	for n in range(l):
+		i=1
+		lst.append([])
+		print(msg+str(n+1))
+		while i!=0:
+			i=validate(int,"")
+			if i>0:
+				lst[n].append(i)
+				print(lst[n])
+				if (sum(lst[n])+len(lst[n])-1)>m:
+					lst[n]=[]
+					print("Overboard, resetting.\n")
+					print(lst[n])
+	return lst
+
+#builds list of block groups, the basis of solutions
+def group(num,l):
+	blocks=[]
+	blocks.append(["O" for x in range(num[0])])
+	for n in num[1:]:
+		blocks.append([" "]+["O" for x in range(n)])
+	return blocks
+
+
+#method for assigning a column
+def col(main,lst,c):
+	for n in range(len(main)):
+		main[n][c]=lst[n]
+	return main
+
+#combine sublists in a list into a single list
+def combine(lst):
+	ret=[]
+	for n in lst:
+		ret=ret+n
+	return ret
+
+#generates a set of solutions for a number list and length
+def solset(num,l):
+	if len(num)==0:
+		return [[" " for n in range(l)]]
+	groups=group(num,l)
+	sol=[]
+	blanks=l-sum(num)-len(num)+1
+	for n in range(len(groups)):
+		print(groups)#debug
+		print(len(groups))#debug
+		for m in range(blanks):
+			s=groups#for reasons beyond me, "groups" seems to change with s
+			print(s)#debug
+			print(groups)#debug
+			s[n]=[" " for x in range(blanks-m)]+s[n]+[" " for x in range(m)]
+			s=combine(s)
+			sol.append(s)
+	return sol
+
+print(solset([1,2],5))#debug
+
+#get dimensions
 r=0
+while r<1:
+	r=validate(int,"Input row length:\n")
+
 c=0
-while r<1 or r%1!=0:
-	try:
-		r=float(input("Please input row length:\n"))
-	except ValueError:
-		pass
-r=int(r)
-while c<1 or c%1!=0:
-	try:
-		c=float(input("Please input column height:\n"))
-	except ValueError:
-		pass
-c=int(c)
+while c<1:
+	c=validate(int,"Input column height:\n")
 
-#construct 2D array of puzzle
-nonogram=[["-" for x in range(r)] for x in range(c)]
+#make the array proper
+nonogram=[["-" for n in range(r)] for m in range(c)]
 
-#construct list of numbers for all rows and columns
-rows=[[] for n in range(c)]
-columns=[[]for n in range(r)]
-
-#get defining numbers for each row and column
-print("Row Input\n")
-for n in range(c):
-	print("Row "+str(n+1)+"; press 0 to advance to next row:\n")
-	i=0
-	while i>0 or len(rows[n])==0:
-		if sum(rows[n])+len(rows[n])-1>r:
-			rows[n]=[]
-			print("Invalid long list.  Resetting")
-			print(rows[n])
-		try:
-			i=int(input())
-			if i>=0:
-				rows[n].append(i)
-				print(rows[n])
-		except:
-			pass
-rows=[[rows[x][y] for y in range(len(rows[x])-1)] for x in range(len(rows))]
-
-for n in range(r):
-	print("Column "+str(n+1)+"; press 0 to advance to next column:\n")
-	i=0
-	while i>0 or len(columns[n])==0:
-		if sum(columns[n])+len(columns[n])-1>r:
-			columns[n]=[]
-			print("Invalid long list.  Resetting")
-			print(columns[n])
-		try:
-			i=int(input())
-			if i>=0:
-				columns[n].append(i)
-				print(columns[n])
-		except:
-			pass
-columns=[[columns[x][y] for y in range(len(columns[x])-1)] for x in range(len(columns))]
-
-#run through each row and column, compare each solution to the one before
-#for each row
-for n in rows:
-	solset=[]
-	comp=[]
-	#for each number in the row
-	for m in n:
-		comp.append(["O" for x in range(m)])
-	#for each group of blocks
-	for a in comp:
-		solset[0]=solset[0]+a
-		if len(solset[0])<r:
-			solset[0].append(0)
+#get a list of number lists for each row and column
+print("Input number lists for rows; 0 ends and advances to next list.\n")
+rows=rnc(c,r,"Row ")
+print("Input number lists for columns; 0 ends and advances to next list.\n")
+columns=rnc(r,c,"Column ")
